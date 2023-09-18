1171437746acc313ce09d341cc14d9810600e125
Uriel Mori <moriuriel@gmail.com>
2023-09-18 13:19:48 -0300

    refactor: creating new folder structure

---
+++ src/PaymentAnalyzer/PaymentAnalyzer.Worker/Program.cs
@@ -0,0 +1,11 @@
+﻿using PaymentAnalyzer.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
+
